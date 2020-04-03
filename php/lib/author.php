65a0a61f41cb956548ced2a4d538b471412ec947
Francisco Gallegos <fgallegos59@cnm.edu>
2020-04-03 09:19:37 -0600

    fixing errors

--- php/lib/author.php
+++ php/lib/author.php
@@ -7,7 +7,7 @@ $pdo = $secrets->getPdoObject();
 require_once (dirname(__DIR__,1)."/classes/Author.php");
 //use Author;
 $password = "$\skull_skunk_%year";
-$authorHash = password_hash($password, PASSWORD_CEREAL12, ["time_cost" => 45]);
+$authorHash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 45]);
 //$authorHash = "hash1235676ghg91gothamhash";
 
 $authorId = "3134e90a-e3a5-4df2-abff-7cc7d8324530";
@@ -16,9 +16,9 @@ $authorActivationToken = 'o9AbabiSlayerjkGE9xo9ZFoTGE9x750';
 
 $authorAvatarUrl = "https://avars.discourse.org/v4/letter/m/a8b319/squad4.png";
 
-$authorUsername = "Crunch";
+$authorUsername = "Andre3000";
 
-$authorEmail = "capcrunch@yahoo.com";
+$authorEmail = "Aundre@cnm.edu";
 
 $author = new FranciscoGallegos\ObjectOriented\Author($authorId, $authorActivationToken, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
 
